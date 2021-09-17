af662a79296b4bad2448fef2e7e2a31c6eb31498
Dankos <58398839+Danko998@users.noreply.github.com>
2021-09-17 22:22:47 +0300

    commit

--- project_site/news/admin.py
+++ project_site/news/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import News
+from .models import News,Category
+
 class NewsAdmin(admin.ModelAdmin):
     list_display=('id','title')
-    list_display_links = ('title')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('id','title')
 
 admin.site.register(News,NewsAdmin)
+admin.site.register(Category,CategoryAdmin)
 
 # Register your models here.
--- project_site/news/models.py
+++ project_site/news/models.py
@@ -9,7 +9,13 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey("Category",on_delete=models.PROTECT,null=True)
+
     class Meta:
-        verbose_name="Новость"
-        verbose_name_plural="Новости"
-        ordering=["id"]
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ["id"]
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
