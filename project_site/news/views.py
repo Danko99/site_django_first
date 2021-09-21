8cf64f9327831e084d7038a02f561b2298e9e6c4
Dankos <58398839+Danko998@users.noreply.github.com>
2021-09-21 22:35:58 +0300

    commit

--- project_site/news/views.py
+++ project_site/news/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 from http import *
+
+
 # Create your views here.
 def index(request):
-    news= News.objects.all()
-    return render(request,'news/index.html',{'news':news,'title':"Список новостей"})
+    news = News.objects.all()
+    categories = Category.objexts.all()
+    context = {
+        'news': news,
+        'title': "Список новостей",
+        "categories": categories
 
+    }
+    return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, template_name='news/category.html',
+                  context={'news': news, "categories": categories, "category": category})
